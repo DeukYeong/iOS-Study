df9997effc04a19a04d8c02a83311c18e7942af0
DeukYeong <polcet112@gmail.com>
2022-09-05 03:40:54 +0900

    날씨앱 만들기 1#

---
+++ Clone Coding/Weather App/Shared/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Shared
+//
+//  Created by 이득령 on 2022/09/05.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
